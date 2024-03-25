97641168dee66e2e73da5d92f91b0b34fdf9481d
Peter Whitesell <peterwhitesell@gmail.com>
2024-03-25 17:30:16 -0500

    add scroll layers to pmw3360

--- kmk/modules/pmw3360.py
+++ kmk/modules/pmw3360.py
@@ -11,50 +11,74 @@ max_int16 = 65536
 _REG_Lift_Config = const(0x63)
 
 class PMW3360(Module):
-    def __init__(self, sck, mosi, miso, cs, invert_x=False, invert_y=False, offset_angle=90, lift_config=0x01, cpi=1000):
-        print(dir(PMW3360Lib))
+    def __init__(self, sck, mosi, miso, cs, invert_x=False, invert_y=False, offset_angle=90, lift_config=0x01, cpi=1000, scroll_layers=[]):
         self.sensor = PMW3360Lib.PMW3360(sck, mosi, miso, cs)
         self.invert_x = invert_x
         self.invert_y = invert_y
         self.offset_angle = math.radians(offset_angle)
         self.lift_config = lift_config
         self.cpi = cpi
+        self.scroll_layers = scroll_layers
+        self.v_scroll_ctr = 0
+        self.h_scroll_ctr = 0
+        self.scroll_res = 10
 
     def during_bootup(self, keyboard):
         if self.sensor.begin(self.cpi):
             if debug.enabled:
-                print("sensor ready")
+                debug("sensor ready")
         else:
             if debug.enabled:
-                print("firmware upload failed")
+                debug("firmware upload failed")
+
     def before_matrix_scan(self, keyboard):
         data = self.sensor.read_burst()
         if not data["is_on_surface"] or not data["is_motion"]:
             return
         raw_x = data["dx"]
         raw_y = data["dy"]
-
         if raw_x > max_int16/2:
             raw_x = raw_x - max_int16
         if raw_y > max_int16/2:
             raw_y = raw_y - max_int16
         # x was coming out inverted
         raw_x = -raw_x
-        
         dx = math.floor(raw_x * math.cos(self.offset_angle) - raw_y * math.sin(self.offset_angle))
         dy = math.floor(raw_x * math.sin(self.offset_angle) + raw_y * math.cos(self.offset_angle))
-
         if self.invert_x:
             dx *= -1
         if self.invert_y:
             dy *= -1
-
-        if keyboard.debug_enabled and (dx != 0 or dy != 0):
-            print('Delta: ', dx, ' ', dy)
+        if dx == 0 and dy == 0:
+            return
+        if keyboard.debug_enabled:
+            debug('Delta: ', dx, ' ', dy)
+        if keyboard.active_layers[0] in self.scroll_layers:
+            self.v_scroll(keyboard, dy)
+            self.h_scroll(keyboard, dx)
+        else:
             if dx:
                 AX.X.move(keyboard, dx)
             if dy:
                 AX.Y.move(keyboard, dy)
+
+    def v_scroll(self, keyboard, delta):
+        self.v_scroll_ctr += delta
+        if self.v_scroll_ctr >= self.scroll_res:
+            AX.W.move(keyboard, -1)
+            self.v_scroll_ctr = 0
+        if self.v_scroll_ctr <= -self.scroll_res:
+            AX.W.move(keyboard, 1)
+            self.v_scroll_ctr = 0
+
+    def h_scroll(self, keyboard, delta):
+        self.h_scroll_ctr += delta
+        if self.h_scroll_ctr >= self.scroll_res:
+            AX.P.move(keyboard, 1)
+            self.h_scroll_ctr = 0
+        if self.h_scroll_ctr <= -self.scroll_res:
+            AX.P.move(keyboard, -1)
+            self.h_scroll_ctr = 0
 
     def after_matrix_scan(self, keyboard):
         return
